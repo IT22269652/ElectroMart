bc8428256a309d5a3377c719878c15629a139633
Nuradha Dakshina <IT22269652@my.sliit.lk>
2025-03-21 09:21:05 +0530

    login backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -5,6 +5,10 @@ const userSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    contactNo: { type: String, required: true }, // New field
+    address: { type: String, required: true }, // New field
+    gender: { type: String, required: true }, // New field
+    birthday: { type: Date, required: true }, // New field
     cartData: { type: Object, default: {} },
   },
   { minimize: false }
